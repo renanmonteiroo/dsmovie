ef0b523a753df8146daa12bd545b1f2daa662504
Renan Monteiro <renan.monteiro01@outlook.com>
2022-03-09 15:53:12 -0300

    MovieCards

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,36 @@
-import Pagination  from "components/Pagination";
-function Listing (){
+import Pagination from "components/Pagination";
+import MovieStars from "components/MovieStars";
+import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 
-    return(
-        <Pagination />
+
+function Listing() {
+
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+            </div>
+        </div>
+
+        </>
     );
 };
 export default Listing;
